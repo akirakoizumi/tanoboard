07ca1788c044a0facf5b1bacc74b5f97c485b542
akirakoizumi <adventureofurban@gmail.com>
2020-06-16 16:35:23 +0900

    userコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get  '/signup', to: 'users#new'
   root to: 'tasks#index'
-  #get 'tasks/index'
+  # get 'tasks/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
