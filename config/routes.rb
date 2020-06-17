30f1a5cf17ddc11ca2047d49f837e0df62c7f8d8
akirakoizumi <adventureofurban@gmail.com>
2020-06-17 22:43:42 +0900

    sessionコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  get  '/signup', to: 'users#new'
+  get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   root to: 'tasks#index'
   # get 'tasks/index'
