547cf6db4670736ac336cb01bda514d67e1678cd
akirakoizumi <adventureofurban@gmail.com>
2020-06-17 17:47:12 +0900

    Userクラスのviewの雛形作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,37 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました"
+      # flash[:success] = 'ユーザー登録が完了しました。'
+      # log_in(@user)
+      # redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
   get  '/signup', to: 'users#new'
+
   root to: 'tasks#index'
   # get 'tasks/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
