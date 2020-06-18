aacd148743b47a29af3bd94a4d6ada238b4500d0
akirakoizumi <adventureofurban@gmail.com>
2020-06-18 15:27:23 +0900

    ユーザー詳細ページにアクセスできるように

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'ログインしました。'
+      redirect_to user_path(user.id), notice: 'ログインしました。'
     else
       render :new
     end
