bb2f9e471abe174b9e6656d0da563784010c4802
akirakoizumi <adventureofurban@gmail.com>
2020-06-21 03:49:50 +0900

    emailのNotNullを外す

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_180853) do
+ActiveRecord::Schema.define(version: 2020_06_20_183446) do
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "group_id", null: false
     t.bigint "user_id", null: false
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_06_20_180853) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "email", null: false
+    t.string "email"
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
