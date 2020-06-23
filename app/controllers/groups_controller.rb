bf7599f86e84479ba770da153e606cad46ddb8fb
akirakoizumi <adventureofurban@gmail.com>
2020-06-23 15:25:19 +0900

    current_userのif文を変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,6 +16,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     if @group.save
       @group.users << current_user
+      log_in(current_user)
       flash[:success] = 'グループを作成しました'
       redirect_to group_path(@group)
     else
