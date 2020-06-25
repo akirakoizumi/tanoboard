a00f1dc535da9590374c29dfd5f1862988569b31
akirakoizumi <adventureofurban@gmail.com>
2020-06-26 00:40:37 +0900

    管理ユーザー判定関数追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def admin?
+    current_user.admin
+  end
+
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
