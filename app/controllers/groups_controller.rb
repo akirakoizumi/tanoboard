0544051057ed5a466b90cbd3fd27a8b96f84e582
akirakoizumi <adventureofurban@gmail.com>
2020-06-26 00:41:39 +0900

    グループ編集機能追加

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,6 +24,20 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash[:success] = 'グループ情報を編集しました。'
+      redirect_to @group
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def group_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+
   resources :users do
     member do
       get :name_edit
@@ -11,13 +12,13 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :groups
+
   get '/signup', to: 'users#new'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  resources :groups, only: [:index, :show, :new, :create]
 
   # get 'tasks/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
