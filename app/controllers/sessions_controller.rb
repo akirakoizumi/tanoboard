7a8264abccbce47d6419888056aae3e7461bfd83
akirakoizumi <adventureofurban@gmail.com>
2020-06-25 19:02:31 +0900

    メールアドレスかIDでログインできるように変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: session_params[:email].downcase)
+    user = User.find_by(id: session_params[:id_or_email].tr("０-９", "0-9")) ||
+      User.find_by(email: session_params[:id_or_email].downcase)
     if user&.authenticate(session_params[:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = 'ログインしました。'
       redirect_to user_path(user.id)
     else
-      flash[:warning] = 'メールアドレスまたはパスワードが違います。'
+      flash[:warning] = 'id、メールアドレスまたはパスワードが違います。'
       render :new
     end
   end
@@ -25,6 +26,6 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.require(:session).permit(:id_or_email, :password)
   end
 end
