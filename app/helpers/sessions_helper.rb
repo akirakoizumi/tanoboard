e542d627001c2b198ed988127cd49894fefb9f47
akirakoizumi <adventureofurban@gmail.com>
2020-06-30 23:56:13 +0900

    自動でグループのSessionを取得

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,11 @@
 module SessionsHelper
+  # ログインする
+  # サブユーザーならグループに入れる
   def log_in(user)
     session[:user_id] = user.id
+    if !user.admin
+      group_in(user.groups.first)
+    end
   end
 
   def logged_in?
@@ -11,12 +16,14 @@ module SessionsHelper
     current_user.admin
   end
 
+  # ログイン情報を保存する
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # ログイン情報を削除する
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -27,5 +34,20 @@ module SessionsHelper
   def log_out
     forget(current_user)
     session.delete(:user_id)
+    session.delete(:group_id)
+  end
+
+  # 自分が所属するグループか？
+  def current_user_belongs_to?(group)
+    current_user.groups.include?(group)
+  end
+
+  # グループ情報を保存する
+  def group_in(group)
+    if current_user_belongs_to?(group)
+      session[:group_id] = group.id
+    else
+      session.delete(:group_id)
+    end
   end
 end
