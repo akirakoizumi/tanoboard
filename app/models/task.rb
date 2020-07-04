c42695007c7c49f3d3638f74834c43a2e3c49fb6
akirakoizumi <adventureofurban@gmail.com>
2020-07-04 16:27:52 +0900

    バリデーションを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
 end
