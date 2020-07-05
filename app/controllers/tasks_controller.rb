37c027aeeca881532b1325ccdfba06d067aff854
akirakoizumi <adventureofurban@gmail.com>
2020-07-05 21:17:50 +0900

    投稿機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,21 @@ class TasksController < ApplicationController
 
   def new
   end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      flash[:success] = 'タスクを登録しました。'
+      redirect_to group_path(session[:group_id])
+    else
+      flash[:danger] = '登録に失敗しました。'
+      redirect_to group_path(session[:group_id])
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :user_id, :group_id)
+  end
 end
