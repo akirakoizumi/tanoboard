aacf29b90508c74bdc624db7bf9ecdb7308eec8c
akirakoizumi <adventureofurban@gmail.com>
2020-07-06 22:04:38 +0900

    reacted_by?関数の作成とタイプミスの修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,13 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  has_many :reactions, dependent: :destroy
+  has_many :reaction, dependent: :destroy
 
   validates :content, presence: true
 
   scope :recent, -> { order(created_at: :desc) }
+
+  def reacted_by?(user)
+    where(user_id: user.id).exists?
+  end
 end
