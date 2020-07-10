bdf1d1e2d93a048fa6f72253b7a1566cd733631a
akirakoizumi <adventureofurban@gmail.com>
2020-07-11 00:19:18 +0900

    ajaxによる了解機能実装

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,13 +1,13 @@
 class ReactionsController < ApplicationController
   def create
+    @task = Task.find(params[:task_id])
     reactions = current_user.reactions.build(task_id: params[:task_id])
     reactions.save
-    redirect_to group_path(current_group)
   end
 
   def destroy
+    @task = Task.find(params[:task_id])
     reactions = Reaction.find_by(task_id: params[:task_id], user_id: current_user.id)
     reactions.destroy
-    redirect_to group_path(current_group)
   end
 end
