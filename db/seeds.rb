6963c6af990ca290d4ade48f611ef256c735eaf6
akirakoizumi <adventureofurban@gmail.com>
2020-07-15 01:09:34 +0900

    seed_fu導入に伴い削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-# t.string "name", null: false
-# t.text "description"
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
-# t.index ["name"], name: "index_groups_on_name", unique: true
-# end
-Group.create!(
-  name: '長崎店',
-  description: '明るく楽しい職場を作ろう！'
-)
-
-# create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-#   t.string "name", null: false
-#   t.string "email"
-#   t.string "password_digest", null: false
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-#   t.boolean "admin", default: false, null: false
-#   t.string "remember_digest"
-#   t.integer "default_group_id"
-#   t.index ["email"], name: "index_users_on_email", unique: true
-# end
-
-User.create!(
-  name: '濱川',
-  email: 'hamakawa@example.com',
-  admin: true,
-  password: 'password',
-  password_confirmation: 'password'
-)
