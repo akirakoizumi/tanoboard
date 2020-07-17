20eb63dbd24f6f8fc065c9b7ea33dc6025c7b88a
akirakoizumi <adventureofurban@gmail.com>
2020-07-17 22:12:40 +0900

    テスト用のseed_fuデータ作成

---
+++ db/fixtures/test/011_user.rb
@@ -0,0 +1,65 @@
+# create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+#   t.string "name", null: false
+#   t.string "email"
+#   t.string "password_digest", null: false
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.boolean "admin", default: false, null: false
+#   t.string "remember_digest"
+#   t.integer "default_group_id"
+#   t.index ["email"], name: "index_users_on_email", unique: true
+# end
+
+User.seed do |s|
+  s.id = 1
+  s.name = '濱川'
+  s.email = 'hamakawa@example.com'
+  s.admin = true
+  s.password = 'password'
+  s.password_confirmation = 'password'
+end
+
+User.seed do |s|
+  s.id = 2
+  s.name = '岩木'
+  s.email = 'iwaki@example.com'
+  s.admin = false
+  s.password = 'password'
+  s.password_confirmation = 'password'
+end
+
+User.seed do |s|
+  s.id = 3
+  s.name = '小野'
+  s.email = 'ono@example.com'
+  s.admin = false
+  s.password = 'password'
+  s.password_confirmation = 'password'
+end
+
+User.seed do |s|
+  s.id = 4
+  s.name = '若林'
+  s.email = 'wakabayasi@example.com'
+  s.admin = false
+  s.password = 'password'
+  s.password_confirmation = 'password'
+end
+
+User.seed do |s|
+  s.id = 11
+  s.name = '北原'
+  s.email = 'kitahara@example.com'
+  s.admin = true
+  s.password = 'password'
+  s.password_confirmation = 'password'
+end
+
+User.seed do |s|
+  s.id = 12
+  s.name = '青山'
+  s.email = 'aoyama@example.com'
+  s.admin = false
+  s.password = 'password'
+  s.password_confirmation = 'password'
+end
