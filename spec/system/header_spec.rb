61a3de5976b8ee602de56fa20f61e89c0720e3d6
akirakoizumi <adventureofurban@gmail.com>
2020-07-20 23:10:52 +0900

    ヘッダーのテストを追加

---
+++ spec/system/header_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'header system spec', type: :system do
+  def login(id)
+    visit login_path
+    login_user = User.find(id)
+    fill_in 'IDまたはメールアドレス', with: login_user.id
+    fill_in 'パスワード', with: 'password'
+    click_button('ログインする')
+  end
+
+  context '管理ユーザーでログインした時' do
+    before do
+      login(1)
+    end
+
+    it 'ユーザー詳細のリンクが表示され移動できる' do
+      click_link 'ユーザー詳細'
+      expect(current_path).to eq user_path(1)
+    end
+
+    it 'グループページへのリンクが表示され移動できる' do
+      click_link 'グループページへ'
+      expect(current_path).to eq group_path(1)
+    end
+
+    it 'ログアウトのリンクが表示され移動できる' do
+      within('.sticky-top') do
+        click_link 'ログアウト'
+        expect(current_path).to eq root_path
+      end
+    end
+  end
+end
