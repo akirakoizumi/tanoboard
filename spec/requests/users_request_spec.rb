364257d71824dee97e091ab6121f777d7b882370
akirakoizumi <adventureofurban@gmail.com>
2020-07-20 16:51:19 +0900

    User情報に関するデータを追加

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,12 +1,69 @@
-# require 'rails_helper'
-#
-# RSpec.describe "Users", type: :request do
-#
-#  describe "GET /new" do
-#    it "returns http success" do
-#      get "/users/new"
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
-#
-# end
+require 'rails_helper'
+
+RSpec.describe type: :request do
+  describe "users_controller" do
+    def login(id)
+      post login_path, params: { session: { id_or_email: id, password: 'password', remember_me: '1' } }
+    end
+
+    context '管理ユーザーでログインした時' do
+      before do
+        login(1)
+      end
+
+      it "IDが表示される" do
+        within('td') do
+          expect(page).to have_content '1'
+        end
+      end
+
+      it "名前が表示される" do
+        within('td') do
+          expect(page).to have_content User.find(1).name
+        end
+      end
+
+      it "emailが表示される" do
+        within('td') do
+          expect(page).to have_content User.find(1).email
+        end
+      end
+
+      it "管理権あり表示される" do
+        within('td') do
+          expect(page).to have_content 'あり'
+        end
+      end
+    end
+
+    context 'サブユーザーでログインした時' do
+      before do
+        login(2)
+      end
+
+      it "IDが表示される" do
+        within('td') do
+          expect(page).to have_content '1'
+        end
+      end
+
+      it "名前が表示される" do
+        within('td') do
+          expect(page).to have_content User.find(1).name
+        end
+      end
+
+      it "emailが表示される" do
+        within('td') do
+          expect(page).to have_content User.find(1).email
+        end
+      end
+
+      it "管理権なしと表示される" do
+        within('td') do
+          expect(page).to have_content 'なし'
+        end
+      end
+    end
+  end
+end
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'session user spec', type: :system do
+  def login(id)
+    visit login_path
+    login_user = User.find(id)
+    fill_in 'IDまたはメールアドレス', with: login_user.id
+    fill_in 'パスワード', with: 'password'
+    click_button('ログインする')
+  end
+
+  context '管理ユーザーでログインした時' do
+    before do
+      login(1)
+    end
+
+    it 'ユーザー情報編集リンクが表示される' do
+      expect(page).to have_link 'ユーザー情報編集'
+    end
+
+    it '名前変更が表示されない' do
+      expect(page).not_to have_link '名前変更'
+    end
+
+    it 'パスワード変更リンクが表示される' do
+      expect(page).to have_link 'パスワード変更'
+    end
+
+    it 'ログアウトリンクが表示される' do
+      expect(page).to have_link 'ログアウト'
+    end
+
+    it '削除リンクが表示される' do
+      expect(page).to have_link '削除'
+    end
+  end
+
+  context 'サブユーザーでログインした時' do
+    before do
+      login(2)
+    end
+
+    it 'ユーザー情報編集リンクが表示されない' do
+      expect(page).not_to have_link 'ユーザー情報編集'
+    end
+
+    it '名前変更が表示される' do
+      expect(page).to have_link '名前変更'
+    end
+
+    it 'パスワード変更リンクが表示される' do
+      expect(page).to have_link 'パスワード変更'
+    end
+
+    it 'ログアウトリンクが表示される' do
+      expect(page).to have_link 'ログアウト'
+    end
+
+    it '削除リンクが表示される' do
+      expect(page).to have_link '削除'
+    end
+  end
+end
