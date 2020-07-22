94557614c93e3d994e0637c72f02edcefe610b3b
akirakoizumi <adventureofurban@gmail.com>
2020-07-22 23:31:11 +0900

    タスク削除に関するテスト追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -16,17 +16,34 @@ describe 'task system spec', type: :system do
       visit group_path(1)
     end
 
+    it '自分が作っていないタスクは削除ボタンが表示されない' do
+      within(".card-body.task-81") do
+        expect(page).not_to have_link '削除'
+      end
+    end
+
     it 'タスクを投稿できる' do
       fill_in 'タスク内容', with: 'テスト用タスク001'
       click_button('登録する')
       expect(page.body).to include 'テスト用タスク001'
     end
 
-    it 'タスクを投稿した後詳細ページにアクセスできる' do
-      fill_in 'タスク内容', with: 'テスト用タスク002'
-      click_button('登録する')
-      click_link 'テスト用タスク002'
-      expect(current_path).to eq task_path(Task.find_by(content: 'テスト用タスク002'))
+    context 'タスクを投稿した後' do
+      before do
+        fill_in 'タスク内容', with: 'テスト用タスク002'
+        click_button('登録する')
+      end
+
+      it '詳細ページにアクセスできる' do
+        click_link 'テスト用タスク002'
+        expect(current_path).to eq task_path(Task.find_by(content: 'テスト用タスク002'))
+      end
+
+      it '一覧のタスクに削除が表示される' do
+        within(".card-body.task-#{Task.find_by(content: 'テスト用タスク002').id}") do
+          expect(page).to have_link '削除'
+        end
+      end
     end
   end
 end
