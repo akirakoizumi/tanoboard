6099d46186ddaa8252d8e69c92cfc408df384af2
akirakoizumi <adventureofurban@gmail.com>
2020-07-22 23:15:18 +0900

    タスクが投稿できないのでbefore_actionを修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :logged_in_user
-  before_action :correct_user, only: %i(create destroy)
+  before_action :correct_user, only: %i(destroy)
   def show
     @task = Task.find(params[:id])
     @users = @task.all_reaction_user
